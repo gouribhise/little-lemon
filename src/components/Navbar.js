d0d6efdb1c859fa7706250051cfffc131dad87ed
gouribhise <gouri.a.bhise@gmail.com>
2024-04-03 19:50:49 +0530

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,45 @@
+import { useState } from 'react'
+import logo from '../images/Logo .svg'
+import hamburger from '../images/🦆 icon _hamburger menu.svg'
 const Navbar =()=>{
+    const [menuOpen,setMenuOpen]=useState(false)
+    const toggleMenu=()=>{
+        setMenuOpen(!menuOpen)
+    }
     return (
-        <div>Navbar</div>
+  <nav className='navbar'>
+    <a href="/">
+        <img src={logo} alt="logo"/>
+    </a>
+    <div className='hamburger-icon'>
+        <img src={hamburger} alt='hamburger icon' onClick={toggleMenu}/>
+
+    </div>
+    <ul className={`nav-links ${menuOpen?"visible":""}`}>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/">About</a>
+        </li>
+        <li>
+            <a href="/">Services</a>
+        </li>
+        <li>
+            <a href="/">Menu</a>
+        </li>
+        <li>
+            <a href="/">Reservations</a>
+        </li>
+        <li>
+            <a href="/">Order Online</a>
+        </li>
+        <li>
+            <a href="/">Login</a>
+        </li>
+    </ul>
+
+  </nav>
     )
 }
 
