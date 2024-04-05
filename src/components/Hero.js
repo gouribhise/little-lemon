b87ad9eb0ddb63b842d01b5c6065f4622e152ead
gouribhise <gouri.a.bhise@gmail.com>
2024-04-05 16:43:46 +0530

    added header section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,18 @@
+import restaurantFood from '../images/restauranfood.jpg'
+import { Link } from 'react-router-dom'
 const Hero =()=>{
     return (
-        <div>Hero</div>
+       <header className="hero">
+<article>
+    <h1>Little Lemon</h1>
+    <h4>Chicago</h4>
+    <p>We are family owned Meditarian restaurant, focused on traditional recipes served with a modern twist.</p>
+    <Link to="/reservations"><button id="reserve_btn">Reserve A Table</button></Link>
+</article>
+ 
+<img src={restaurantFood} alt='restaurant food image'/>
+ 
+       </header>
     )
 }
 
